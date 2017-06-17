92a7d5947f1382e0edcc92cfcd872f709a63e96a
Johan Kämpe <kampe.johan@gmail.com>
2017-06-17 12:45:56 +0200

    Hello World!

---
+++ 002/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿/*   C# Fundamentals for Absolute Beginners
+ *   2 | Creating Your First C# Program
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*   ReadLine to keep console window 
+             *   open until key is pressed   */
+            Console.ReadLine();
+        }
+    }
+}
