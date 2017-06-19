777abaeefda33ca9da090cc1b3f89a41e662d5bf
Johan Kämpe <kampe.johan@gmail.com>
2017-06-19 13:31:43 +0200

    for iteration

---
+++ 008/forIteration/forIteration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Iterate trough 0 to 9, 
+             * print out values to console */
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("i is seven!");
+                    /* Break / end for-loop */
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
