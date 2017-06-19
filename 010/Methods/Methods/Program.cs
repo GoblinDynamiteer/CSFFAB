5863cb25816d4a5aacaf21d2170af0f8a3c30d82
Johan Kämpe <kampe.johan@gmail.com>
2017-06-19 14:27:07 +0200

    Simple HelloWorld method

---
+++ 010/Methods/Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿/*   C# Fundamentals for Absolute Beginners
+ *   10 | Defining and Calling Methods
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Call method */
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        /* A method */
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
