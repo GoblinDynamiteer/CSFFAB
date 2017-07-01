78a962711b6c3e82db2347f45797b6f387650b03
Johan Kämpe <kampe.johan@gmail.com>
2017-07-01 21:10:10 +0200

    Namespaces, NET framework, WebClient, File class

---
+++ 017/AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+using System.IO;
+using System.Net; // Auto inserted by CTRL + . on WebClient clas
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Namespace System */
+            System.Console.WriteLine("Hellow WOrld!");
+
+            string text = "Hellow WOrld!!! In a textfile!";
+
+            // System.IO.
+            File.WriteAllText(@"D:\Temp\CSHARP\WriteText.txt", text);
+
+            /* Press CTRL + . on Class to find assembly reference */
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://www.arla.se");
+            System.Console.WriteLine(reply);
+
+            System.Console.ReadLine();
+        }
+    }
+}
