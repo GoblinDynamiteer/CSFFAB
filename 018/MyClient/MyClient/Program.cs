a7e2ec22dd8e847575bd161ac150f9a97e4c0700
Johan Kämpe <kampe.johan@gmail.com>
2017-07-01 21:48:43 +0200

    dll creation and use

---
+++ 018/MyClient/MyClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MyCodeLibrary; // dll file, added in solution explorer
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+
+            string value = myScrape.ScrapeWebpage("http://www.fz.se");
+            Console.WriteLine(value);
+
+            myScrape.ScrapeWebpage("http://www.arla.se", @"D:\temp\CSHARP\webscrape.txt");
+
+
+            Console.ReadLine();
+        }
+    }
+}
